b5c6ab276c7d5d4a0601886edb3e1d9eb2c04d0a
Laura Marinescu <laurasilvia@outlook.com>
2024-10-26 14:13:11 -0400

    Update speechToText.py

--- speechToText/speechToText.py
+++ speechToText/speechToText.py
@@ -3,8 +3,10 @@ from scipy.io.wavfile import write
 import wavio as wv
 
 import whisper
+from whisper.utils import get_writer
 model = whisper.load_model('base')
 
+#Creating the audio file
 freq = 44100
 duration = 5
 
@@ -13,3 +15,12 @@ sd.wait()
 
 write("recording0.wav", freq, recording)
 wv.write("recording1.wav", recording, freq, sampwidth=2)
+
+#Importing audio file into whisper
+def get_transcribe(audio:'recording1.wav', language: str = 'en'):
+    return model.transcribe(audio = audio, language=language, verbose = True)
+
+#Save the file
+def save_file(results, format = 'txt'):
+    writer = get_writer(format, './output/')
+    writer(results, f'transcribe.{format}')
